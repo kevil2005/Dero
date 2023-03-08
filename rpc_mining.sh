e4428ea379978c65c1f1b9f5f92399c61d51f48e
kevil2005 <127235581+kevil2005@users.noreply.github.com>
2023-03-08 09:48:01 +0700

    Add files via upload

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qygaqcw8dzfmckjy2wz0r4gyyca0ma3vguwlpr0kqayqq3u0fdfqkqgr2yu89 -r 192.168.1.25:10100 -r1 community-pools.mysrv.cloud:10300 -r2 dero.friendspool.club:10300 -p rpc
+    ./astrominer -w dero1qy40tftrmdj8vs7vg56hsh3ta8ue53z5tqfh8ehrx9zz9658tg0sxqggtgefq -r 192.168.1.25:10100 -r1 community-pools.mysrv.cloud:10300 -r2 dero.friendspool.club:10300 -p rpc
     sleep 5;
 done
